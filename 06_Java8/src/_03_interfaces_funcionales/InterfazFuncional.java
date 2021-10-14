60c193a3a9cb1abfd0a01fdae6eb00c2e6817311
Felix de Pablo <f.depablo.lobo@gmail.com>
2021-10-14 11:16:04 +0200

    actividad

---
+++ 06_Java8/src/_03_interfaces_funcionales/InterfazFuncional.java
@@ -0,0 +1,9 @@
+package _03_interfaces_funcionales;
+
+@FunctionalInterface 
+public interface InterfazFuncional {
+	void metodoAbstracto();
+	//Si tuvieramos otro metodo daria error porque no corresponderia a interfaz
+	//funcional
+	//void otroMetodoAbstracto();
+}
--- Ej05_Primos/src/main/Main.java
+++ Ej05_Primos/src/main/Main.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-	 	Scanner sc= new Scanner (System.in);
-		System.out.println("Escribe 3 numeros: ");
-		
-		IsPrime primo = new IsPrime();
-		primo.setNum(sc.nextInt());
-		Thread t1 = new Thread(primo);
-		
-		IsPrime primo2 = new IsPrime();
-		primo2.setNum(sc.nextInt());
-		Thread t2 = new Thread(primo2);
-		
-		IsPrime primo3 = new IsPrime();
-		primo3.setNum(sc.nextInt());
-		Thread t3 = new Thread(primo3);
-	 
-		t1.start();
-		t2.start();
-		t3.start();
-
+		//Al poner el scanner en un bloque try-with-resource no hace
+		//falta invocar el .close(), lo hace automaticamente
+	 	try(Scanner sc= new Scanner (System.in);){
+			System.out.println("Escribe 3 numeros: ");
+			
+			IsPrime primo = new IsPrime();
+			primo.setNum(sc.nextInt());
+			Thread t1 = new Thread(primo);
+			
+			IsPrime primo2 = new IsPrime();
+			primo2.setNum(sc.nextInt());
+			Thread t2 = new Thread(primo2);
+			
+			IsPrime primo3 = new IsPrime();
+			primo3.setNum(sc.nextInt());
+			Thread t3 = new Thread(primo3);
+		 
+			t1.start();
+			t2.start();
+			t3.start();
+	 	}
 	}
 
 }
